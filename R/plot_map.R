c2069610ad903144812315cb84f72fcb9056153a
Pedro Pinto da Silva <ppintodasilva@gmail.com>
2020-02-03 15:49:43 +0000

    Add function and tests for plotting spatial data as a geographical map.

---
+++ R/plot_map.R
@@ -0,0 +1,150 @@
+# Default color ---------------------------
+
+cameras_col <- "N 3/0"
+flows_col <- "5YR 6/6"
+primary_col <- "5BG 6/4"
+arterial_col <- "5BG 8/2"
+polygon_col <- "N 9/0"
+
+# Functions ---------------------------
+
+#' Plot traffic demand over time, grouped by location.
+#'
+#' @importFrom magrittr %>%
+#' @importFrom ggplot2 ggplot aes geom_line geom_point scale_x_datetime theme_bw
+#' ggtitle
+#' @param spatial List of spatial features.
+#' @param flows Optional summarised flows$l tibble.
+#' @param add_primary Whether to plot primary network.
+#' @param add_arterial Whether to plot arterial network.
+#' @param add_paths Whether to plot shortest paths between locations.
+#' @param color_primary Color of primary network.
+#' @param color_arterial Color of arterial network.
+#' @param color_locations Color of locations.
+#' @param color_paths Color of paths.
+#' @param size_primary Line size of the primary network.
+#' @param size_arterial Line size of the arterial network.
+#' @param size_paths Line size of pahts.
+#' @param size_locations Size of locations.
+#' @param locations_palette Brewer palette for locations when used together
+#' with aes_color_locations.
+#' @param aes_color_locations Color aesthetic to use for locations as
+#'  a character.
+#' @param aes_color_flows Color aesthetic to use for paths as a character.
+plot_map <- function(
+  spatial,
+  flows = NULL,
+  add_primary = TRUE, add_arterial = FALSE,
+  add_paths = TRUE, add_locations = TRUE,
+  color_primary = munsell::mnsl(primary_col),
+  color_arterial = munsell::mnsl(arterial_col),
+  color_locations = munsell::mnsl(cameras_col),
+  color_paths = munsell::mnsl(flows_col),
+  size_primary = .2,
+  size_arterial = .1,
+  size_paths = .5,
+  size_locations = 3,
+  aes_color_locations = "",
+  locations_palette = "Set2",
+  aes_color_flows = ""
+) {
+
+  # If elses applied to data frames
+  if(is.null(spatial$primary$edges)) {
+    primary <- spatial$primary
+  } else {
+    primary <- spatial$primary$edges
+  }
+
+  if(is.null(spatial$arterial$edges)) {
+    arterial <- spatial$arterial
+  } else {
+    arterial <- spatial$arterial$edges
+  }
+
+  if(aes_color_locations == "") {
+    locations <- spatial$locations
+  } else {
+    locations <- spatial$locations %>% st_buffer(30)
+  }
+
+  if(aes_color_flows == "") {
+    pairs <- spatial$pairs
+  } else if("geometry" %in% names(flows)){
+    pairs <- flows
+  } else {
+    pairs <-
+      suppressWarnings(
+        inner_join(
+          flows %>% filter(o != "SOURCE", d != "SINK"),
+          spatial$pairs
+        )
+      )
+  }
+
+  # Plot time
+  ggplot() +
+    # primary network
+    {
+      if(add_primary) {
+        geom_sf(
+          data = primary,
+          mapping = aes(geometry = geometry),
+          color = color_primary,
+          size = size_primary
+        )
+      }
+    } +
+    {
+      if(add_arterial) {
+        geom_sf(
+          data = arterial,
+          mapping = aes(geometry = geometry),
+          color = color_arterial,
+          size = size_primary
+        )
+      }
+    } +
+    {
+      if(add_paths & aes_color_flows == "") {
+        geom_sf(
+          data = pairs,
+
+          mapping = aes(geometry = geometry),
+          color = color_paths,
+          size = size_paths
+        )
+      }
+    } +
+    {
+      if(add_paths & aes_color_flows != "") {
+        geom_sf(
+          data = pairs,
+          mapping = aes(geometry = geometry,
+                        color = !!sym(aes_color_flows)),
+          size = size_paths
+        )
+      }
+    } +
+    {
+      if(add_locations) {
+        geom_sf(
+          data = locations,
+          mapping = aes(geometry = geometry,
+                        fill = !!sym(aes_color_locations)),
+          color = ifelse(aes_color_locations == "", color_locations, NA),
+          size = size_locations
+        )
+      }
+    } +
+    {
+      if(aes_color_locations != "") {
+        scale_fill_brewer(palette = locations_palette, name = "location")
+      }
+    } +
+    theme(
+      panel.grid = element_blank(),
+      axis.line = element_blank()
+    )
+
+}
---
+++ tests/testthat/test-plot-map.R
@@ -0,0 +1,59 @@
+context("plot-map")
+
+spatial_1 <- readr::read_rds(spatial1_filename)
+raw_flows_1 <- read_flows_csv(filenames = test1_filename)
+
+summarised_raw_flows <-
+  raw_flows_1 %>%
+  group_by(o,d) %>%
+  summarise(flow = sum(flow))
+
+summarised_spatial_flows_1 <-
+  suppressWarnings(dplyr::left_join(spatial_1$pairs, raw_flows_1)) %>%
+  group_by(o,d) %>%
+  summarise(flow = sum(flow))
+
+# plot_map performs left join, colored locations, colored flows
+p_1 <-
+  plot_map(
+    spatial = spatial_1,
+    flows = summarised_raw_flows,
+    aes_color_locations = "id",
+    aes_color_flows = "flow")
+
+# input with previously performed left join, colored flows
+p_2 <-
+  plot_map(
+    spatial = spatial_1,
+    flows = summarised_spatial_flows_1,
+    aes_color_locations = "",
+    aes_color_flows = "flow")
+
+# input with previously performed left join, colored flows, add arterial
+p_3 <-
+  plot_map(
+    spatial = spatial_1,
+    flows = summarised_spatial_flows_1,
+    aes_color_locations = "",
+    aes_color_flows = "flow",
+    add_arterial = TRUE)
+
+# no need for flows, just plotting spatial data
+p_4 <- plot_map(spatial_1,
+                aes_color_locations = "id",
+                locations_palette = "Set1")
+
+# plot spatial data with arterial, remove locations and pairs
+p_5 <- plot_map(spatial_1,
+                add_arterial = TRUE,
+                add_locations = FALSE,
+                add_paths = FALSE)
+
+
+test_that("plot maps works", {
+  vdiffr::expect_doppelganger("map with left join colors",p_1)
+  vdiffr::expect_doppelganger("map not left join color flow", p_2)
+  vdiffr::expect_doppelganger("map not left join color flow arterial", p_3)
+  vdiffr::expect_doppelganger("no flows color locations palette", p_4)
+  vdiffr::expect_doppelganger("no flows/color/locations/flows with arterial",p_5)
+})
