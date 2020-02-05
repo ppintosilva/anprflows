e29cb55b5587b8ff6586d252f8088baefe7c2f89
Pedro Pinto da Silva <ppintodasilva@gmail.com>
2020-02-05 19:58:57 +0000

    Add function to plot small network graph from ods
    Moves ggplot2 and ggraph to depends - otherwise ggraph does not work correctly - took me ages to find why and to fix.

---
+++ R/plot_graph.R
@@ -0,0 +1,94 @@
+#' Plot flow network.
+#'
+#' @param flows_od Summarised or windowed od flows tibble.
+#'
+#' @export
+#'
+#' @importFrom magrittr %>%
+#' @importFrom tibble has_name
+plot_small_network <- function(
+  flows_od,
+  ignore_source_sink = FALSE,
+  num_accuracy = .1,
+  num_scale = 1,
+  graph_layout = "auto",
+  aes_edge_color = "",
+  aes_edge_label = "flow",
+  edge_angle_calc = 'along',
+  edge_label_dodge = ggplot2::unit(2.5, 'mm'),
+  edge_arrow = arrow(length = ggplot2::unit(1.5, 'mm')),
+  edge_start_cap = circle(1, 'mm'),
+  edge_end_cap = circle(2, 'mm'),
+  edge_width = 1,
+  edge_alpha = .75,
+  edge_color_palette = "Blues",
+  node_size = 4,
+  node_color = munsell::mnsl("5BG 6/4"),
+  aes_node_label = "name",
+  node_label_nudge_y = 0.1,
+  node_label_nudge_x = 0
+) {
+
+  if(has_name(flows_od, 't')) {
+    if(length(unique(flows_od$t)) > 1) {
+      stop(paste0(
+      "Input flow data must be summarised or windowed ",
+      "(length(unique(flows_od$t)) == 1)"))
+    }
+  }
+
+  graph <-
+    flows_od %>%
+    ungroup() %>%
+    {
+      if(ignore_source_sink) {
+        filter(., o != "SOURCE", d != "SINK")
+      } else .
+    } %>%
+    {
+      if(aes_edge_label != "") {
+        mutate(., label = scales::number(!!sym(aes_edge_label),
+                                         accuracy = num_accuracy,
+                                         scale = num_scale))
+      } else .
+    } %>%
+    {
+      if(has_name(., 't')) {
+        select(., -t)
+      } else .
+    } %>%
+    rename(from = o, to = d) %>%
+    tidygraph::as_tbl_graph()
+
+  label_mapping <- ifelse(aes_edge_label != "", "label", "")
+
+  graph %>%
+    ggraph(layout = graph_layout) +
+    geom_edge_fan(
+      aes(
+        colour = !!sym(aes_edge_color),
+        label = !!sym(label_mapping)
+        ),
+      angle_calc = edge_angle_calc,
+      label_dodge = edge_label_dodge,
+      arrow = edge_arrow,
+      start_cap = edge_start_cap,
+      end_cap = edge_end_cap,
+      edge_width = edge_width,
+      edge_alpha = edge_alpha
+    ) +
+    geom_node_point(
+      size = node_size,
+      colour = node_color
+    ) +
+    {
+      if(aes_node_label != "") {
+        geom_node_text(
+          aes(label = !!sym(aes_node_label)),
+          nudge_y = node_label_nudge_y,
+          nudge_x = node_label_nudge_x
+        )
+      }
+    } +
+    ggplot2::scale_colour_brewer(edge_color_palette)
+}
---
+++ tests/testthat/test-plot-network.R
@@ -0,0 +1,77 @@
+context("plot-network")
+
+# Data ----
+
+test1_filename <-
+  system.file("testdata","small_flows_dataset_set1.csv", package="anprflows")
+
+spatial1_filename <-
+  system.file("testdata","spatial_1.rds", package="anprflows")
+
+raw_flows <- read_flows_csv(filenames = test1_filename)
+
+# summary of raw flows
+summarised_raw_flows <-
+  raw_flows %>%
+  dplyr::group_by(o,d) %>%
+  dplyr::summarise(flow = sum(flow))
+
+# one time slice of raw flows
+raw_flows_8am_slice <-
+  raw_flows %>%
+  dplyr::filter(lubridate::hour(t) == 8 & lubridate::minute(t) == 15)
+
+# processed flows
+flows_l <- get_flows_l(raw_flows)
+flows_od <- get_flows_od(raw_flows, flows_l)
+
+# one time slice of processed flows
+flows_od_8am_slice <-
+  flows_od %>%
+  dplyr::filter(lubridate::hour(t) == 8 & lubridate::minute(t) == 15)
+
+
+# Plots ----
+
+p1a <- plot_small_network(summarised_raw_flows,
+                          num_scale = 0.001,
+                          ignore_source_sink = TRUE)
+p1b <- plot_small_network(summarised_raw_flows,
+                          num_accuracy = NULL,
+                          aes_edge_label = "flow",
+                          aes_edge_color = "flow")
+
+p2a <- plot_small_network(raw_flows_8am_slice,
+                          ignore_source_sink = TRUE)
+p2b <- plot_small_network(raw_flows_8am_slice,
+                          aes_edge_color = "flow")
+
+p3a <- plot_small_network(flows_od_8am_slice,
+                          aes_edge_color = "rate_o",
+                          aes_edge_label = "rate_o",
+                          num_accuracy = .01)
+p3b <- plot_small_network(flows_od_8am_slice,
+                          aes_edge_label = "rate_d",
+                          num_accuracy = .01)
+p3c <- plot_small_network(flows_od_8am_slice,
+                          aes_edge_label = "",
+                          aes_edge_color = "rate_d")
+
+# Tests ----
+
+test_that("plot small network works", {
+  vdiffr::expect_doppelganger(
+    "small network 1 summarised a", p1a)
+  vdiffr::expect_doppelganger(
+    "small network 1 summarised b", p1b)
+  vdiffr::expect_doppelganger(
+    "small network 2 raw slice a", p2a)
+  vdiffr::expect_doppelganger(
+    "small network 2 raw slice b", p2b)
+  vdiffr::expect_doppelganger(
+    "small network 3 slice a", p3a)
+  vdiffr::expect_doppelganger(
+    "small network 3 slice b", p3b)
+  vdiffr::expect_doppelganger(
+    "small network 3 slice c",p3c)
+})
