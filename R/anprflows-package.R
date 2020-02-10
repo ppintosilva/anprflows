bb7c6ce51220e18a2c4b0c0b80a4fe88ddecc6e2
Pedro Pinto da Silva <ppintodasilva@gmail.com>
2020-02-10 15:50:30 +0000

    Add function to get subgraphs from flow network
    Extend flow network function to optionally label subgraphs

--- R/anprflows-package.R
+++ R/anprflows-package.R
@@ -1,11 +1,11 @@
 ## usethis namespace: start
-#' @importFrom tibble tibble
+#' @importFrom tibble tibble as_tibble
 #' @importFrom assertr verify
 #' @importFrom dplyr select arrange filter rename mutate group_by ungroup desc
 #' @importFrom dplyr distinct pull summarise inner_join full_join everything
-#' @importFrom dplyr bind_rows sym
+#' @importFrom dplyr bind_rows sym if_else left_join
 #' @importFrom tidyr pivot_longer pivot_wider replace_na
-#' @importFrom tidygraph as_tbl_graph
+#' @importFrom tidygraph as_tbl_graph activate
 #' @importFrom ggraph ggraph geom_edge_fan geom_node_point geom_node_text
 #' @importFrom stats setNames
 #' @importFrom utils tail
--- R/network.R
+++ R/network.R
@@ -8,7 +8,8 @@
 #'
 flow_network <- function(
   flows,
-  spurious_thresholds = c("rate_o" = .1, "rate_d" = .1)
+  spurious_thresholds = c("rate_o" = .1, "rate_d" = .1),
+  label_subgraphs = TRUE
 ) {
 
   # If column 't' exists it should have a single distinct value
@@ -26,5 +27,39 @@ flow_network <- function(
     filtered_flows <- flows
   }
 
-  filtered_flows %>% as_tbl_graph()
+  filtered_flows %>%
+    as_tbl_graph() %>%
+    {
+      if(label_subgraphs) {
+        activate(., nodes) %>%
+          left_join(
+            get_subgraphs(.),
+            by = c("name" = "name")
+          )
+      }
+    }
+}
+
+
+#' Label nodes according to the subgraph, if any, that they belong in.
+#'
+#' @param network A flow network
+#'
+#' @export
+#'
+get_subgraphs <- function(network) {
+  communities <- igraph::decompose.graph(network)
+
+  1:length(communities) %>%
+  lapply(function(x)
+          communities[[x]] %>%
+          as_tbl_graph() %>%
+          activate(nodes) %>%
+          as_tibble() %>%
+          mutate(subgraph = x)) %>%
+    bind_rows() %>%
+    mutate(
+      subgraph = ifelse(.data$name == "SOURCE" | .data$name == "SINK",
+                        -1, .data$subgraph)
+    )
 }
