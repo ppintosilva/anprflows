8f0f8e8419fc39fe2e316a4ce58192a0c183050d
Pedro Pinto da Silva <ppintodasilva@gmail.com>
2020-02-10 16:33:27 +0000

    Add option to not consider sink and source nodes in subgraph labelling
    Add subgraph test and fix warnings

--- R/network.R
+++ R/network.R
@@ -1,25 +1,27 @@
 #' Create a tidygraph from flows and remove spurious edges in the process.
 #'
 #' @param flows Asymptotic od flows
-#' @param spurious_thresholds Remove edges whose attributes are lower than
+#' @param spurious_if_below Remove edges whose attributes are lower than
 #' the corresponding threshold.
+#' @param label_subgraphs Whether to label subgraphs as a result of removing
+#' spurious edges.
 #'
 #' @export
 #'
 flow_network <- function(
   flows,
-  spurious_thresholds = c("rate_o" = .1, "rate_d" = .1),
+  spurious_if_below = c("rate_o" = .1, "rate_d" = .1),
   label_subgraphs = TRUE
 ) {
 
   # If column 't' exists it should have a single distinct value
   stop_if_multiple_time_steps(flows)
 
-  if(!is.null(spurious_thresholds)) {
+  if(!is.null(spurious_if_below)) {
     filtered_flows <-
       lapply(
-        names(spurious_thresholds),
-        function(x) flows %>% filter(!!sym(x) > spurious_thresholds[x])
+        names(spurious_if_below),
+        function(x) flows %>% filter(!!sym(x) > spurious_if_below[x])
       ) %>%
       bind_rows() %>%
       distinct(.data$o,.data$d, .keep_all = TRUE)
@@ -31,7 +33,7 @@ flow_network <- function(
     as_tbl_graph() %>%
     {
       if(label_subgraphs) {
-        activate(., nodes) %>%
+        activate(., "nodes") %>%
           left_join(
             get_subgraphs(.),
             by = c("name" = "name")
@@ -44,22 +46,31 @@ flow_network <- function(
 #' Label nodes according to the subgraph, if any, that they belong in.
 #'
 #' @param network A flow network
+#' @param ignore_source_sink Whether to ignore source and sink nodes in label
+#' calculation.
 #'
 #' @export
 #'
-get_subgraphs <- function(network) {
-  communities <- igraph::decompose.graph(network)
+get_subgraphs <- function(network, ignore_source_sink = TRUE) {
+  communities <-
+    network %>%
+    {
+      if(ignore_source_sink) {
+        igraph::delete.vertices(., c("SOURCE", "SINK"))
+      }
+    } %>%
+    igraph::decompose.graph()
 
   1:length(communities) %>%
   lapply(function(x)
           communities[[x]] %>%
           as_tbl_graph() %>%
-          activate(nodes) %>%
+          activate("nodes") %>%
           as_tibble() %>%
           mutate(subgraph = x)) %>%
     bind_rows() %>%
     mutate(
       subgraph = ifelse(.data$name == "SOURCE" | .data$name == "SINK",
-                        -1, .data$subgraph)
+                        NA, .data$subgraph)
     )
 }
--- tests/testthat/test-network.R
+++ tests/testthat/test-network.R
@@ -1,7 +1,12 @@
 test1_filename <-
   system.file("testdata","small_flows_dataset_set1.csv", package="anprflows")
 
+test2_filename <-
+  system.file("testdata","small_flows_dataset_set2.csv", package="anprflows")
+
 raw_flows_1 <- read_flows_csv(filenames = test1_filename)
+
+raw_flows_2 <- read_flows_csv(filenames = test2_filename)
 
 asympt_flows_1_l <- get_flows_l(raw_flows_1,
                                 by_period = FALSE)
@@ -9,14 +14,28 @@ asympt_flows_1_l <- get_flows_l(raw_flows_1,
 asympt_flows_1_od <- get_flows_od(raw_flows_1, asympt_flows_1_l,
                                   by_period = FALSE)
 
+# default thresholds = 0.1, 0.1
+G1 <- flow_network(asympt_flows_1_od)
+
+# Both flows together
+raw_flows   <-
+  read_flows_csv(filenames = c(test1_filename,test2_filename)) %>%
+  dplyr::arrange(o,d,t)
+
+asympt_flows_l <- get_flows_l(raw_flows,
+                              by_period = FALSE)
+
+asympt_flows_od <- get_flows_od(raw_flows, asympt_flows_l,
+                                by_period = FALSE)
+
+G <- flow_network(asympt_flows_od,
+                  label_subgraphs = TRUE,
+                  spurious_if_below = c("rate_o" = .10))
 
 test_that("flow network works", {
 
-  # thresholds = 0.1, 0.1
-  G <- flow_network(asympt_flows_1_od)
-
   observed_nrow <-
-    G %>%
+    G1 %>%
     tidygraph::activate(edges) %>%
     tibble::as_tibble() %>%
     nrow()
@@ -25,4 +44,16 @@ test_that("flow network works", {
   expected_nrow <- 6
 
   expect_true(observed_nrow == expected_nrow)
+})
+
+test_that("flow network with two subgraphs is labelled correctly", {
+
+  subgraphs <-
+    G %>%
+    activate(nodes) %>%
+    as_tibble() %>%
+    distinct(subgraph) %>%
+    pull(subgraph)
+
+  expect_equal(subgraphs, c(1,2,NA))
 })
