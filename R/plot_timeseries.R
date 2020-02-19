6782ecec8b25ea9e735cf9a268b6fdbb17e8b1e9
Pedro Pinto da Silva <ppintodasilva@gmail.com>
2020-02-19 11:27:16 +0000

    Add further control arguments to od plot demand and speed functions

--- R/plot_timeseries.R
+++ R/plot_timeseries.R
@@ -65,19 +65,24 @@ plot_demand_l <- function(
     ggplot2::theme_bw()
 }
 
-#' Plot traffic demand over time, grouped by od pair.
+#' Plot traffic demand over time
 #'
 #' @param flows_od Trimmed flows$od tibble.
 #' @param time_breaks Breaks for scale_x_datetime
-#' @param point_alpha Alpha parameter of point layer
-#' @param point_size Size parameter of point layer
-#' @param line_size Size parameter of line layer
-#' @param include_source_sink Include or exclude edges from source or to sink
-#' nodes.
+#' @param add_points whether to include point layer
+#' @param aes_color color aesthetic
+#' @param point_alpha alpha parameter of point layer
+#' @param point_size size parameter of point layer
+#' @param line_size size parameter of line layer
+#' @param include_source_sink include or exclude edges from source or to sink
+#' nodes
+#'
 #' @export
 #'
 plot_demand_od <- function(
   flows_od, time_breaks = NULL,
+  add_points = TRUE,
+  aes_color = "od",
   point_alpha = .5, point_size = .75, line_size = .5,
   include_source_sink = FALSE
 ) {
@@ -94,19 +99,23 @@ plot_demand_od <- function(
       ggplot2::aes(
         x = .data$t,
         y = .data$flow,
-        color = .data$od
+        color = !!sym(aes_color)
       ),
       size = line_size
     ) +
-    ggplot2::geom_point(
-      ggplot2::aes(
-        x = .data$t,
-        y = .data$flow,
-        color = .data$od
-      ),
-      alpha = point_alpha,
-      size = point_size
-    ) +
+    {
+      if(add_points) {
+        ggplot2::geom_point(
+          ggplot2::aes(
+            x = .data$t,
+            y = .data$flow,
+            color = !!sym(aes_color)
+          ),
+          alpha = point_alpha,
+          size = point_size
+        )
+      }
+    } +
     {
       if(!is.null(time_breaks)) {
         ggplot2::scale_x_datetime(
@@ -117,25 +126,32 @@ plot_demand_od <- function(
     } +
     ggplot2::xlab("Time") +
     ggplot2::ylab("Flow") +
-    ggplot2::labs(color = "OD pair") +
+    {
+      if(aes_color != "")
+        ggplot2::labs(color = "OD pair")
+    } +
     ggplot2::theme_bw()
 }
 
 #' Plot traffic speed over time, grouped by od pair.
 #'
-#' @param flows_od Trimmed flows$od tibble.
-#' @param time_breaks Breaks for scale_x_datetime
-#' @param point_alpha Alpha parameter of point layer
-#' @param point_size Size parameter of point layer
-#' @param add_ribbon Whether to add ribbon representing deviation from the mean
-#' @param ribbon_alpha Alpha parameter of ribbon layer
-#' @param ribbon_fill Fill parameter of ribbon layer
-#' @param line_size Size parameter of line layer
+#' @param flows_od trimmed flows$od tibble
+#' @param time_breaks breaks for scale_x_datetime
+#' @param add_points whether to include point layer
+#' @param aes_color color aesthetic
+#' @param point_alpha alpha parameter of point layer
+#' @param point_size size parameter of point layer
+#' @param add_ribbon whether to add ribbon representing deviation from the mean
+#' @param ribbon_alpha alpha parameter of ribbon layer
+#' @param ribbon_fill fill parameter of ribbon layer
+#' @param line_size size parameter of line layer
 #'
 #' @export
 #'
 plot_speed_od <- function(
   flows_od, time_breaks = NULL,
+  add_points = TRUE,
+  aes_color = "od",
   point_alpha = .5, point_size = .75,
   add_ribbon = TRUE, ribbon_alpha = .4, ribbon_fill = "grey90",
   line_size = .5
@@ -156,7 +172,7 @@ plot_speed_od <- function(
             x = .data$t,
             ymin = .data$mean_speed - .data$sd_speed,
             ymax = .data$mean_speed + .data$sd_speed,
-            color = .data$od
+            color = !!sym(aes_color)
           ),
           fill = ribbon_fill,
           linetype = 0,
@@ -167,19 +183,23 @@ plot_speed_od <- function(
       ggplot2::aes(
         x = .data$t,
         y = .data$mean_speed,
-        color = .data$od
+        color = !!sym(aes_color)
         ),
       size = line_size
     ) +
-    ggplot2::geom_point(
-      ggplot2::aes(
-        x = .data$t,
-        y = .data$mean_speed,
-        color = .data$od
-      ),
-      alpha = point_alpha,
-      size = point_size
-    ) +
+    {
+      if(add_points) {
+        ggplot2::geom_point(
+          ggplot2::aes(
+            x = .data$t,
+            y = .data$mean_speed,
+            color = !!sym(aes_color)
+          ),
+          alpha = point_alpha,
+          size = point_size
+        )
+      }
+    } +
     {
       if(!is.null(time_breaks)) {
         ggplot2::scale_x_datetime(
@@ -190,6 +210,10 @@ plot_speed_od <- function(
     } +
     ggplot2::xlab("Time") +
     ggplot2::ylab("Mean speed") +
-    ggplot2::labs(color = "OD pair") +
+    {
+      if(aes_color != "")
+        ggplot2::labs(color = "OD pair")
+    } +
     ggplot2::theme_bw()
 }
+
