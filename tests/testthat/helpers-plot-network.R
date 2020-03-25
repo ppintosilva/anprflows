9b9f6c994d7eb25f172f14def0af69234d7ed4e1
Pedro Pinto da Silva <ppintodasilva@gmail.com>
2020-03-25 18:16:13 +0000

    Suppress ggraph messages in helpers-plot-network

--- tests/testthat/helpers-plot-network.R
+++ tests/testthat/helpers-plot-network.R
@@ -1,45 +1,61 @@
 # ---- Test different combinatoins of slice and raw/processed flow networks
 
 p_asympt_no_source_sink_simple <-
-  plot_small_network(G1_asympt,
-                     include_source_sink = FALSE,
-                     aes_edge_label = "")
+  suppressMessages(
+    plot_small_network(G1_asympt,
+                       include_source_sink = FALSE,
+                       aes_edge_label = "")
+  )
 
 p_asympt_no_source_sink <-
-  plot_small_network(G1_asympt,
-                     num_scale = 0.001,
-                     aes_edge_label = "flow",
-                     include_source_sink = FALSE)
+  suppressMessages(
+    plot_small_network(G1_asympt,
+                       num_scale = 0.001,
+                       aes_edge_label = "flow",
+                       include_source_sink = FALSE)
+  )
 
 p_asympt_no_spurious <-
-  plot_small_network(G1_asympt_no_spurious,
-                     num_scale = 0.001,
-                     aes_edge_label = "flow",
-                     include_source_sink = TRUE)
+  suppressMessages(
+    plot_small_network(G1_asympt_no_spurious,
+                       num_scale = 0.001,
+                       aes_edge_label = "flow",
+                       include_source_sink = TRUE)
+  )
 
 p_8am_slice <-
-  plot_small_network(G1_8am_slice,
-                     num_accuracy = NULL,
-                     aes_edge_label = "flow",
-                     aes_edge_color = "flow",
-                     include_source_sink = TRUE) +
-  ggplot2::labs(caption="weighted by flow")
+  suppressMessages(
+    plot_small_network(G1_8am_slice,
+                       num_accuracy = NULL,
+                       aes_edge_label = "flow",
+                       aes_edge_color = "flow",
+                       include_source_sink = TRUE) +
+    ggplot2::labs(caption="weighted by flow")
+  )
 
 p_8am_slice_no_source_sink <-
-  plot_small_network(G1_8am_slice,
-                     num_accuracy = NULL,
-                     aes_edge_label = "rate_o",
-                     include_source_sink = TRUE) +
-  ggplot2::labs(caption="weighted by rate_o")
+  suppressMessages(
+    plot_small_network(G1_8am_slice,
+                       num_accuracy = NULL,
+                       aes_edge_label = "rate_o",
+                       include_source_sink = TRUE) +
+    ggplot2::labs(caption="weighted by rate_o")
+  )
 
 
 p_2communities <-
-  plot_small_network(G_asympt, include_source_sink = TRUE)
+  suppressMessages(
+    plot_small_network(G_asympt, include_source_sink = TRUE)
+  )
 
 p_2communities_no_source_sink <-
-  plot_small_network(G_asympt, include_source_sink = FALSE)
+  suppressMessages(
+    plot_small_network(G_asympt, include_source_sink = FALSE)
+  )
 
 p_multiple_small_plots <-
-  G_asympt %>%
-  small_network_plots(num_scale = 0.001, aes_node_fill = "") %>%
-  patchwork::wrap_plots(ncol = 2)
+  suppressMessages(
+    G_asympt %>%
+    small_network_plots(num_scale = 0.001, aes_node_fill = "") %>%
+    patchwork::wrap_plots(ncol = 2)
+  )
